884e3e34457ed0936cb826c316417c9da9c7ec7e
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-01-26 20:00:46 +0530

    refactor : minor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,19 @@
+import dotenv from "dotenv";
 import config from "config";
-import { app } from "./app";
 import logger from "./utils/logger.utils";
+import { app } from "./app";
 import { Config } from "../config/default";
+
+// Configure the dotenv to access the environment variables
+dotenv.config({
+	path: ".env",
+});
 
 // Get the config details for the app.
 const { port, domain } =
 	config.get<Config["server"]["connection"]>("server.connection");
 
 // Start the express app.
-app.listen(+port, domain, () => {
+app.listen(port, domain, () => {
 	logger.info(`Server running on port : ${port}`);
 });
