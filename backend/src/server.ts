2b8fbd1253add3947313b775f9ed107347f98443
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-01-27 16:02:03 +0530

    add: another method to connect with db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import config from "config";
-import logger from "./utils/logger.utils";
+import mongoose from "mongoose";
+import logger, { logEvents } from "./utils/logger.utils";
 import { app } from "./app";
 import { Config } from "../config/default";
 import { connectDB } from "./db/connect";
@@ -15,12 +16,37 @@ const { port, domain } =
 	config.get<Config["server"]["connection"]>("server.connection");
 
 // Start the express app.
+
+// Method 1 -
+// connectDB();
+
+// mongoose.connection.once("open", () => {
+// 	app.listen(port, domain, () => {
+// 		logger.info(`Server running on port : ${port}`);
+// 	});
+// });
+
+// mongoose.connection.once("error", (error) => {
+// 	logger.info("Server did not start!");
+// 	logEvents(
+// 		`${error.no}: ${error.code}\t${error.syscall}\t${error.hostname}`,
+// 		"mongoErrLog.log",
+// 		"error"
+// 	);
+// });
+
+// Method 2 -
 connectDB()
 	.then(() => {
 		app.listen(port, domain, () => {
 			logger.info(`Server running on port : ${port}`);
 		});
 	})
-	.catch(() => {
-		logger.warn("Server did not start!");
+	.catch((error) => {
+		logger.info("Server did not start!");
+		logEvents(
+			`${error.no}: ${error.code}\t${error.syscall}\t${error.hostname}`,
+			"mongoErrLog.log",
+			"error"
+		);
 	});
